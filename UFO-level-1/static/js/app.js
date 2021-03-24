43062948e517d980094defc608964bcf643573c8
jaryan77 <j.alexryan178@gmail.com>
2021-03-24 18:01:58 -0500

    level 1 complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,54 +3,64 @@ var tableData = data;
 
 // YOUR CODE HERE
 // Create variable for table body to put the output data into
-var $tbody = d3.select("tbody");
-
+var tbody = d3.select("tbody");
+// var form = d3.select("form")
 // Select the filter button
 // not sure if this would be #button or 'filter-btn'
-var button = d3.select("#filter-btn");
+// var button = d3.select("#filter-btn");
 
-
-
-// referencing for inpur date from line 21 in html
-var inputElement = d3.select("#datetime");
-
-// name event handler function
-button.on("click", runEnter);
-form.on("submit",runEnter);
+// // name event handler function
+// button.on("click", runEnter);
+// form.on("submit",runEnter);
 
 // Use function from above
-function runEnter() {
+function runEnter(data) {
     //Prevent page from refreshing
-    d3.event.preventDefault();
+    // d3.event.preventDefault();
+    tbody.html("");
 
+    data.forEach((dataRow) => {
+        var row = tbody.append("tr"); 
+    
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+              cell.text(val);
+            }
+        );
+    });
+}
+
+function clickButton() {
     //Select the input element and get the raw html node (explain?),
     //   use id="datetime" from line 40 w/ input tag
     //  ----- moving this code out of function
-    //var inputElement = d3.select("#datetime");
+    var inputElement = d3.select("#datetime");
 
     // Get value property from input element
     var inputValue = inputElement.property("value");
-
+    var filteredData = tableData.filter(row => row.datetime === inputValue);
+    
+    console.log(filteredData);
     console.log(inputValue);
     console.log(tableData);
 
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-
-    console.log(filteredData);
-
+    runEnter(filteredData);
     //Select list element by class name?? referencing "table-area" line 51 in html
-    var list = d3.select("");
+    // var list = d3.select("");
 
-    list.html("");
+    // list.html("");
 
-    list.append("tr").text(`${datetime}`);
-    list.append("tr").text(`${city}`);
-    list.append("tr").text(`${state}`);
-    list.append("tr").text(`${country}`);
-    list.append("tr").text(`${shape}`);
-    list.append("tr").text(`${durationMinutes}`);
-    list.append("tr").text(`${comements}`);
+    // list.append("tr").text(`${datetime}`);
+    // list.append("tr").text(`${city}`);
+    // list.append("tr").text(`${state}`);
+    // list.append("tr").text(`${country}`);
+    // list.append("tr").text(`${shape}`);
+    // list.append("tr").text(`${durationMinutes}`);
+    // list.append("tr").text(`${comements}`);
 };
+
+d3.selectAll("#filter-btn").on("click",clickButton);
+runEnter(tableData);
 
 
 
